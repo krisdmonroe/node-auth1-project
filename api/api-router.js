22882cf29f7bbf23809f55b9c106f8de6dac8011
Kris Monroe <krisdmonroe@gmail.com>
2020-02-25 09:08:50 -0800

    last commit

--- api/api-router.js
+++ api/api-router.js
@@ -5,7 +5,7 @@ const authRouter = require("../auth/auth-router.js");
 const usersRouter = require("../users/users-router.js");
 const restricted = require("../auth/restricted-middleware.js");
 
-router.use("/auth", authRouter);
+router.use("", authRouter);
 router.use("/users", restricted, usersRouter);
 
 router.get("/hash", (req, res) => {
