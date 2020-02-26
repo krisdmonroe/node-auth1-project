be63dcc473c81dc8f214ed65bf951513501be60d
Kris Monroe <krisdmonroe@gmail.com>
2020-02-26 13:57:27 -0800

    Fixes

--- api/configure-middleware.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-
-
-module.exports = server => {
-  server.use(express.json());
-};
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,21 +1,22 @@
-const bcrypt = require("bcryptjs"); // <<<<<<<<<<<<<<<< npm i bcryptjs
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
 const Users = require("../users/users-model.js");
 
+// for endpoints beginning with /api/auth
 router.post("/register", (req, res) => {
   let user = req.body;
-
-  const hash = bcrypt.hashSync(user.password, 8); // << 1
-
-  user.password = hash; // <<<<<<<<<<<<<<<<<<<<<<<<<<<< 2
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
+      req.session.loggedIn = true;
+
       res.status(201).json(saved);
     })
     .catch(error => {
-      res.status(500).json({error: 'you shall not pass'});
+      res.status(500).json(error);
     });
 });
 
@@ -26,14 +27,35 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: 'Login Succesful' });
+        req.session.loggedIn = true;
+        req.session.username = user.username;
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+        });
       } else {
-        res.status(401).json({ message: "You shall not pass!" });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({
+          you: "can check out any time you like, but you can never leave",
+        });
+      } else {
+        res.status(200).json({ you: "logged out successfully" });
+      }
+    });
+  } else {
+    res.status(200).json({ bye: "felicia" });
+  }
 });
 
 module.exports = router;
